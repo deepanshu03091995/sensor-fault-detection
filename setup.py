5e101d96a411ff272d87bb8d11548c9e50f324e7
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2022-10-28 22:27:53 +0530

    structure code setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function returns the list of requirements
+    """
+    requirements_list:List[str]=[]
+    return requirements_list
+
+
+
+
+
+
+
+
+
+
+
+
+setup(name='Sensor',
+      version='0.0.1',
+      description='Sensor fault detection System',
+      author='Deepanshu rajput',
+      author_email='201812009@daiict.ac.in',
+      packages=find_packages(),
+      install_requires=[],
+     )
