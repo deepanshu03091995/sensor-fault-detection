fe2dd6db0e04d1a4251dc7978e32890dfd9e62fa
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2022-11-03 22:09:01 +0530

    data ingestion

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -1,6 +1,6 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
-
+from sklearn.model_selection import train_test_split
 from sensor.entity.config_entity import DataIngestionConfig
 from sensor.entity.artifact_entity import DataIngestionArtifact
 import os, sys
@@ -37,7 +37,25 @@ class DataIngestion:
     def split_data_as_train_test(self, dataframe: DataFrame) -> None:
         """split data of feature store into train and test"""
 
-        pass
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+            logging.info("Performing train test split on the dataframe")
+            logging.info("exited split_train_test method of data ingestion class")
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            logging.info("Exporting train and test file path")
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+            test.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+            logging.info(f"Exported train and test file path.")
+
+        except Exception as e:
+            raise SensorException(e, sys)
 
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
--- sensor/configuration/mongodb_connection.py
+++ sensor/configuration/mongodb_connection.py
@@ -1,5 +1,9 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
+from dotenv import load_dotenv
+
+load_dotenv()
 import certifi
 import os
 
@@ -13,8 +17,8 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME):
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv("MONGODB_URL_KEY")
-                # mongo_db_url = "mongodb+srv://Deepadmin:Root@cluster0.mhel9.mongodb.net/admin?authSource=admin&replicaSet=atlas-6qsgpk-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+                # mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = "mongodb+srv://Deepadmin:Root@cluster0.mhel9.mongodb.net/admin?authSource=admin&replicaSet=atlas-6qsgpk-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
                 if mongo_db_url is None:
                     raise Exception(f"Environment Key :{MONGODB_URL_KEY} id not set")
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
